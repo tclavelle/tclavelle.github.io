0220414ea8ec01d09f4da7005c668f5f82480844
tclavelle <tclavelle@bren.ucsb.edu>
2017-05-21 23:17:55 -0700

    publishing phish post

--- blog/scripts/phish_in_helpers.R
+++ blog/scripts/phish_in_helpers.R
@@ -1,34 +1,40 @@
 library(tidyverse)
 library(httr)
 library(stringr)
+library(wordcloud2)
 
 # Get data from phish.in API for song info
-song_res <- GET('http://phish.in/api/v1/songs.json?per_page=10000')
-song_raw <- song_res %>% content %>% .$data
-song_counts <- map_df((seq_along(song_raw)), function(x) {
-  list(song         = song_raw[[x]]$title,
-       song_id      = song_raw[[x]]$id,
-       performances = song_raw[[x]]$tracks_count)
-})
+# song_res <- GET('http://phish.in/api/v1/songs.json?per_page=10000')
+# song_raw <- song_res %>% content %>% .$data
+# song_counts <- map_df((seq_along(song_raw)), function(x) {
+#   list(song         = song_raw[[x]]$title,
+#        song_id      = song_raw[[x]]$id,
+#        performances = song_raw[[x]]$tracks_count)
+# })
 
-# Get data from phish.in API for tracks
-res <- GET('http://phish.in/api/v1/tracks.json?per_page=10000')
-raw <- res %>% content %>% .$data 
-  
+# # Get data from phish.in API for tracks
+track_info <- function(track_id) {
+res <- GET(paste0('http://phish.in/api/v1/tracks/',track_id,'.json?per_page=10000'))
+raw <- res %>% content %>% .$data
+
 # Convert to data_frame
 track_df <- map_df(seq_along(raw), function(x) {
-  list(song       = raw[[x]]$title,
-       song_id    = raw[[x]]$song_ids[1],
-       track_id   = raw[[x]]$id,
-       set        = raw[[x]]$set_name,
-       length     = raw[[x]]$duration / 1000 / 60,
-       mp3        = raw[[x]]$mp3,
-       slug       = raw[[x]]$slug)
+  list(song       = raw$title,
+       song_id    = raw$song_ids[[1]],
+       track_id   = raw$id,
+       set        = raw$set_name,
+       date       = raw$show_date,
+       position   = raw$position,
+       length     = raw$duration / 1000 / 60,
+       mp3        = raw$mp3,
+       slug       = raw$slug)
 })
+return(track_df)
+}
   
 # Write function to get data for all performances of a desired song
-song_info <- function(track_id) {
-  track <- GET(paste0('http://phish.in/api/v1/songs/',track_id,'.json?per_page=1000'))
+song_info <- function(song_id) {
+  track <- GET(paste0('http://phish.in/api/v1/songs/',song_id,'.json?per_page=1000'))
   raw <- track %>% content %>% .$data %>% .$tracks 
   
   # Convert to data_frame
@@ -45,23 +51,121 @@ song_info <- function(track_id) {
   return(song_df)
 }
 
-# pull out all performances for every song with more than 100 performances
-top_songs <- lapply(song_counts$song_id[song_counts$performances>300], song_info) %>%
-  bind_rows()
+# Write function to get data for locations of all performances
+venue_info <- function(venue_id) {
+  # venue <- GET(paste0('http://phish.in/api/v1/venues/',venue_id,'.json?per_page=500'))
+  venues <- GET(paste0('http://phish.in/api/v1/venues.json?per_page=1000'))
+  raw <- venues %>% content %>% .$data
+  
+  # Convert to data_frame
+  venue_df <- map_df(seq_len(length(raw)), function(x) {
+    temp_venue <- list(venue_name = raw[[x]]$name,
+         location   = raw[[x]]$location,
+         shows      = raw[[x]]$shows_count)
+    
+  }) %>%
+    separate(location, into = c('city', 'state'), sep = ', ')
+  
+  return(venue_df)
+}
 
-# song summary (use to determine which jams to explore)
-top_song_summary <- top_songs %>%
-  group_by(song) %>%
-  summarize(count = length(song),
-            avg_length = mean(length, na.rm = T),
-            sd_length  = sd(length, na.rm = T)) %>%
-  filter(grepl('>', song) == F)
+venue_ids <- GET(paste0('http://phish.in/api/v1/venues.json?per_page=1000'))
 
-# calculate summary statistics for each song
-top_songs %>%
-  filter(song %in% c('You Enjoy Myself', 'Tweezer', 'Reba', 'Chalk Dust Torture', 'David Bowie',
-                     "Suzy Greenberg", 'Runaway Jim', 'Possum', 'Harry Hood')) %>%
-  left_join(top_song_summary) %>%
-  ggplot(aes(x = length, fill = song)) +
-  geom_histogram(binwidth = 1) +
-  facet_wrap(~song, scales = 'free')
+venue_ids %>% content %>% .$data
+
+# # pull out all performances for every song in top jams
+# top_songs <- lapply(song_counts$song_id[song_counts$song %in% top_jams], song_info) %>%
+#   bind_rows()
+# 
+# # song summary (use to determine which jams to explore)
+# top_song_summary <- top_songs %>%
+#   group_by(song) %>%
+#   summarize(count = length(song),
+#             avg_length = mean(length, na.rm = T),
+#             sd_length  = sd(length, na.rm = T)) %>%
+#   filter(grepl('>', song) == F & is.na(sd_length)==F) %>%
+#   mutate(type_2 = avg_length + sd_length)
+# 
+# # save data for offline work
+# # write_csv(top_songs, path = '../My-code/phishfromvt/www/phish_in.csv')
+# 
+# # plot distributions
+# top_songs %>%
+#   filter(song %in% top_jams) %>%
+#   left_join(top_song_summary) %>%
+#   mutate(is_type_2 = length >= type_2) %>%
+#   ggplot(aes(x = length, fill = is_type_2)) +
+#   geom_histogram(binwidth = 1) +
+#   geom_vline(data = filter(top_song_summary, song %in% top_jams),
+#              aes(xintercept = avg_length),
+#              linetype = 2) +
+#   scale_fill_brewer(palette = 'Set1') +
+#   facet_wrap(~song, scales = 'free_y')
+# 
+# # Average length per year lineplot
+# top_songs %>%
+#   filter(song %in% top_jams) %>%
+#   group_by(song, date) %>%
+#   summarize(length = sum(length, na.rm = T)) %>%
+#   separate(date, into = c('year', 'month', 'day'), sep = '-') %>%
+#   mutate(year = as.numeric(year)) %>%
+#   group_by(song, year) %>%
+#   summarize(avg_length = mean(length, na.rm = T),
+#             count      = length(song)) %>%
+#   complete(year=full_seq(year, period = 1), nesting(song), fill = list(0)) %>%
+#   ungroup() %>%
+#   ggplot(aes(x = year, y = avg_length, group = song)) +
+#   geom_rect(aes(xmin = min(year, na.rm = T), xmax = 2000, 
+#                 ymin = 0, ymax = max(avg_length, na.rm = T)),
+#             fill = 'lightgreen', alpha = 0.2) +
+#   geom_rect(aes(xmin = 2002, xmax = 2004, 
+#                 ymin = 0, ymax = max(avg_length, na.rm = T)),
+#             fill = 'orange', alpha = 0.2) +
+#   geom_rect(aes(xmin = 2009, xmax = 2017, 
+#                 ymin = 0, ymax = max(avg_length, na.rm = T)),
+#             fill = 'lightblue', alpha = 0.2) +
+#   geom_line() +
+#   geom_point(aes(size = count)) +
+#   scale_size_continuous(breaks = c(seq(10,60,by = 10))) +
+#   facet_wrap(~song) +
+#   theme_bw()
+
+# # Pull out Mike's Groove examples and make word plot of the groove songs
+# ms_groove <- lapply(song_counts$song_id[grepl("Mike's", song_counts$song) | 
+#                                           grepl('Weekapaug', song_counts$song)], song_info) %>%
+#   bind_rows() %>%
+#   arrange(date, position) %>% # arrange by date and track_id to find the in between songs
+#   group_by(date) %>% 
+#   complete(track_id = full_seq(track_id, period = 1)) %>% # expand the track ids to add in the ids of the tracks in between
+#   ungroup()
+#   
+# # find the songs in the groove
+# ms_groove_2 <- lapply(ms_groove$track_id[is.na(ms_groove$song)], track_info) %>%
+#   bind_rows()
+# 
+# # Pull out groove
+# ms_groove_3 <- ms_groove %>%
+#   filter(is.na(song)==F) %>%
+#   bind_rows(ms_groove_2) %>%
+#   arrange(date, track_id) %>%
+#   distinct() %>%
+#   filter(!song %in% c("Mike's Song", "Weekapaug Groove"))
+# 
+# # Fix songs with mike's and paug in the song title  
+# ms_groove_3$song <- gsub(" > Weekapaug Groove", replacement = '', ms_groove_3$song)
+# ms_groove_3$song <- gsub("Mike's Song >", replacement = '', ms_groove_3$song)
+# ms_groove_3$song <- gsub(" I Am Hydrogen", replacement = 'I Am Hydrogen', ms_groove_3$song)
+# 
+# ms_groove_wc <- ms_groove_3 %>%
+#   group_by(song) %>%
+#   summarize(freq = length(song)) %>%
+#   ungroup() %>%
+#   rename(word = song) 
+
+# figPath <- system.file("examples/cactus.png",package = "wordcloud2")
+# wordcloud2(subset(ms_groove_wc, freq > 1), color = rep(c('green','darkgreen'),
+#                                      length.out = nrow(ms_groove_wc)),
+#            size = 0.5)
+
+# Phish.net jam charts
+
--- blog/scripts/spotify_helpers.R
+++ blog/scripts/spotify_helpers.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(httr)
 library(stringr)
+library(lubridate)
 
 ## Search for artists by name
 get_artists <- function(artist_name) {
@@ -21,17 +22,7 @@ get_artists <- function(artist_name) {
     return(artists)
 }
 
-# artist_info <- get_artists('radiohead')
-
-# str(artist_info)
-
-# Filter out other artist matches
-# artist_info <- artist_info %>% 
-#     filter(artist_name == 'Radiohead')
-
 ## Get artist albums
-library(lubridate)
-
 get_albums <- function(artist_uri, lim = 50, off = 0) {
     albums <- GET(paste0('https://api.spotify.com/v1/artists/', artist_uri,'/albums?album_type=album&limit=',lim,'&offset=',off)) %>% content
     
@@ -54,14 +45,6 @@ get_albums <- function(artist_uri, lim = 50, off = 0) {
     }) %>% filter(!duplicated(tolower(album_name))) %>%  # Sometimes there are multiple versions (just with different capitalizations) of the same album
         arrange(album_release_year)
 }
-
-# album_info <- get_albums(artist_info$artist_uri)
-
-# str(album_info)
-
-# Filter out remixes and EPs
-# non_studio_albums <- c('TKOL RMX 1234567', 'In Rainbows Disk 2', 'Com Lag: 2+2=5', 'I Might Be Wrong')
-# album_info <- filter(album_info, !album_name %in% non_studio_albums)
 
 ## Get tracks
 get_tracks <- function(artist_info, album_info, id, secret) {
@@ -106,28 +89,121 @@ get_tracks <- function(artist_info, album_info, id, secret) {
     return(track_info)
 }
 
-
-## Get tracks
-# get_playlist <- function(user_id, playlist_id, id, secret) { 
-#   
-#   client_id <- id
-#   client_secret <- secret
-#   access_token <- POST('https://accounts.spotify.com/api/token',
-#                        accept_json(), authenticate(client_id, client_secret),
-#                        body = list(grant_type='client_credentials'),
-#                        encode = 'form', httr::config(http_version=2)) %>% content %>% .$access_token
-#   
-#   tracks <- GET(paste0('https://api.spotify.com/v1/users/', user_id,'/playlists/', playlist_id, '/tracks?fields=tracks.items(track(name,href,album(name,href)))'),
-#                 query = list(access_token = access_token)) %>% 
-#       
-#     
-#     
+# ### Phish.net helper functions
+# # API and user keys
+# api_key <- 'B53003FE66383F8D79E7'
+# app_key <- '0A3D9E14542B42F37A5E'
 # 
+# # API endpoint
+# end_point <- 'https://api.phish.net/v3/'
 # 
-#   return(df)
+# # Get table of all jamcharts
+# jc_all <- GET(paste0(end_point,'jamcharts/all?apikey=',api_key)) %>% content 
+# jc_all <- jc_all$response[2]$data
+# 
+# # Convert response to data frame
+# jc_all <- map_df(seq_len(length(jc_all)), function(x) {
+#   list(song = jc_all[[x]]$song,
+#        songid = jc_all[[x]]$songid,
+#        jams   = jc_all[[x]]$items
+#   )
 # })
 
+# top_jams <- c('You Enjoy Myself', 'Tweezer', 'Bathtub Gin', 'Chalk Dust Torture', 'David Bowie',
+#               "Down with Disease", 'Runaway Jim', 'Twist', 'Piper', 'Ghost', 'Also Sprach Zarathustra',
+#               'Light')
+# 
+# # Get info for top songs
+# top_jam_ids <- filter(jc_all, song %in% top_jams)
 
+# Function to get individual jamcharts
+jam_chart <- function(songid, apikey = api_key) {
+  
+  jc <- POST(paste0(end_point,'jamcharts/get?songid=',songid,'&apikey=',api_key)) %>% content %>% .$response %>% .$data %>% .$entries
+  
+  # Clean response and combine all returned artists into a dataframe
+  jc2 <- map_df(seq_len(length(jc)), function(x) {
+    list(songid = songid,
+         date = jc[[x]]$showdate,
+         highly_rec = jc[[x]]$marked_recommended)
+  }) %>% bind_rows()
+
+  return(jc2)
+}
+
+# # Pull jamcharts for top_jam_ids
+# top_jams_charts <- lapply(top_jam_ids$songid, FUN = jam_chart) %>%
+#   bind_rows() %>%
+#   left_join(top_jam_ids)
+
+# test <- top_songs %>%
+#   filter(song %in% top_jams) %>%
+#   left_join(top_jams_charts)
+
+client_id <- '8ec1c22eb38d4e0b9e10e8b715a5f74e'
+client_secret <- '5636a032ece24f38aad8cbd01b266ec2'
+
+## Get tracks
+get_playlist <- function(user_id, playlist_id, id, secret, off) {
+
+  # client_id <- id
+  # client_secret <- secret
+  access_token <- POST('https://accounts.spotify.com/api/token',
+                       accept_json(), authenticate(id, secret),
+                       body = list(grant_type='client_credentials'),
+                       encode = 'form', httr::config(http_version=2)) %>% content %>% .$access_token
+  tracks <- GET(paste0('https://api.spotify.com/v1/users/', user_id,'/playlists/', playlist_id,'/tracks?offset=',off),
+                query = list(access_token = access_token)) %>% content %>% .$items 
+  
+  tracks <- map_df(seq_len(length(tracks)), function(x) {
+    list(
+      track_name       = tracks[[x]]$track$name, 
+      album_name       = tracks[[x]]$track$album$name,
+      track_popularity = tracks[[x]]$track$popularity,
+      track_href       = tracks[[x]]$track$href,
+      track_uri        = tracks[[x]]$track$uri
+      )
+  }) 
+  
+  return(tracks)
+}
+
+# Find Phish's artist id info
+# phish <- get_artists('Phish') %>%
+#   filter(artist_name == 'Phish')
+# 
+# # Get all albums. Need to make several calls because there are more than 50 albums
+# phish_albums <- get_albums(artist_uri = phish$artist_uri) %>%
+#   bind_rows(get_albums(artist_uri = phish$artist_uri, off = 50))
+# 
+# # Studio albums to ignore
+# studio <- c('The Siket Disc','The Story Of The Ghost', 'Farmhouse', 'Round Room', 'Joy', 'Fuego', 'Big Boat',
+#             'Undermind', 'Junta', '(Hoist)', 'Billy Breathes', 'Rift', 'A Picture Of Nectar', 'Lawn Boy',
+#             'The White Tape')
+# 
+# # Filter to only live albums
+# livephish <- filter(phish_albums, !album_name %in% studio)
+# 
+# # Get track info
+# live_tracks <- get_tracks(artist_info = phish, id = client_id, secret = client_secret, album_info = livephish)
+# 
+# # Clean track names
+# live_tracks$track_name <- gsub(pattern = ' - Live', replacement = '', live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " [Live At Madison Square Garden, New Year's Eve 1995]", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " [Live In Brooklyn]", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " - Hampton, 1998", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " - Clifford Ball, 1994", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " [Live At Madison Square Garden]", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " [Live At Madison Square Garden]", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " [Live Version - Clifford Ball, 1994]", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " 1", replacement = '', fixed = TRUE, live_tracks$track_name)
+# live_tracks$track_name <- gsub(pattern = " 2", replacement = '', fixed = TRUE, live_tracks$track_name)                            
+# # Save data for offline anaysis
+# # write_csv(live_tracks, path = 'data/spotify_phish_tracks.csv')
+
+# spotify:user:1265043326:playlist:3FdZNyZ42vMB5szof9EA3N
+# test <- get_playlist(user_id = '1265043326', playlist_id = '3FdZNyZ42vMB5szof9EA3N',
+#                      id = client_id, secret = client_secret)
   # spotify_df <- get_tracks(artist_info, album_info)
 
 # str(spotify_df)
